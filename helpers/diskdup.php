8573753247f658e17ebafdc8e496dac3f8c129df
dezi <dezi@kappa-mm.de>
2014-04-02 19:04:07 +0200

    Check in.

--- helpers/diskdup.php
+++ helpers/diskdup.php
@@ -1,8 +1,16 @@
 <?php
 
-$if = fopen("/dev/mmcblk0","r");
-$of = fopen("/dev/sda","w");
+$dv = readlink("/dev/disk/by-label/rootfs");
+if (substr($dv,0,6) != "../../") exit(1);
+
+$id = "/dev/mmcblk0";
+$od = "/dev/" . substr($dv,6,-1);
 $bs = 10 * 1024 * 1024;
+
+echo "dd if=$id of=$od bs=$bs\n";
+
+$if = fopen($id,"r");
+$of = fopen($od,"w");
 $bc = 0;
 
 while (! feof($if))
