f4f324d42b437b63bfaba12f6c27d30a3c61e2e6
dezi <dezi@kappa-mm.de>
2014-04-02 13:28:15 +0200

    Check in.

--- helpers/diskdup.php
+++ helpers/diskdup.php
@@ -2,17 +2,17 @@
 
 $if = fopen("/dev/mmcblk0","r");
 $of = fopen("/dev/sda","w");
-$bs = 1024 * 1024;
+$bs = 10 * 1024 * 1024;
 $bc = 0;
 
-while (true)
+while (! feof($if))
 {
 	$buffer = fread($if,$bs);
 	if ($buffer === false) break;
 
 	fwrite($of,$buffer,strlen($buffer));
 
-	$bc++;
+	$bc += 10;
 
 	echo str_pad($bc,6," ",STR_PAD_LEFT) .  " MB\r";
 }
